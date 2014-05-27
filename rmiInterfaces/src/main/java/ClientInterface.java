f12b5db82d8a5d334663f0247eef5dc631492f5b
thergbway <thergbworld@gmail.com>
2014-05-27 19:35:07 +0400

    Added server RMI support

--- rmiInterfaces/src/main/java/ClientInterface.java
+++ rmiInterfaces/src/main/java/ClientInterface.java
@@ -1,3 +1,17 @@
+import java.util.Date;
+import java.util.List;
+
 public interface ClientInterface {
-    String sayHello();
+    /**
+     * Возвращает актуальный на момент запроса список вариантов для автозаполнения
+     * @return список вариантов для автозаполнения
+     */
+    AutoCompletionDataSnapshot getAutoCompletionDataSnapshot();
+
+    /**
+     * Отправляет на сервер данные введенные на форме для того, чтобы
+     * эти данные были обработаны и сохранены, если нужно.
+     * @param data данные
+     */
+    void offerTypedDataToSave(DataTypedOnTheForm data);
 }
